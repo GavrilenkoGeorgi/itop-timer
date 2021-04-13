df0e941f22023ead084059800d9990a2e2fedbfd
Gavrilenko <gavrilenko.georgi@gmail.com>
2021-04-13 16:15:52 +0300

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders timer', () => {
+	render(<App />)
+	const linkElement = screen.getByText(/00:00:00/i)
+	expect(linkElement).toBeInTheDocument()
+})
